551fd01c37554af643042d1c73f9281015b78b57
Alec Rust <me@alecrust.com>
2024-03-17 15:08:23 +0000

    Delete WooCommerce product meta on uninstall

--- uninstall.php
+++ uninstall.php
@@ -11,7 +11,20 @@ if (!defined('WP_UNINSTALL_PLUGIN')) {
     exit();
 }
 
-// Delete the plugin options from the wp_options table
+// Delete main plugin options from the wp_options table
 delete_option('brevwoo_brevo_api_key');
 delete_option('brevwoo_default_brevo_lists');
 delete_option('brevwoo_order_status_trigger');
+
+// Delete WooCommerce product meta
+if (class_exists('WooCommerce')) {
+    $args = [
+        'post_type' => 'product',
+        'posts_per_page' => -1,
+        'fields' => 'ids',
+    ];
+    $products = get_posts($args);
+    foreach ($products as $product_id) {
+        delete_post_meta($product_id, 'brevwoo_brevo_list_ids');
+    }
+}
