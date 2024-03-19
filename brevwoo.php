0a4a071397a2d89dc3ae13d4ebb45ce86036c90b
Alec Rust <me@alecrust.com>
2024-03-19 19:27:50 +0000

    Remove WooCommerce plugin dependency for now
    This doesn't seem a very mature feature, it causes confusing behavior in the plugins list, and causes e.g. an update of WooCommerce plugin to fail as it can't be removed during the upgrade process.
    
    https://make.wordpress.org/core/2024/03/05/introducing-plugin-dependencies-in-wordpress-6-5/

--- brevwoo.php
+++ brevwoo.php
@@ -4,7 +4,6 @@
  * Plugin Name:       BrevWoo
  * Plugin URI:        http://github.com/AlecRust/brevwoo
  * GitHub Plugin URI: AlecRust/brevwoo
- * Requires Plugins:  woocommerce
  * Description:       Simple and reliable WooCommerce to Brevo integration.
  * Version:           0.0.5
  * Author:            Alec Rust
